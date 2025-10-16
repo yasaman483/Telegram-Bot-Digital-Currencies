b87fb23534e017f27bd49902581ad3da16afe631
yasaman483 <yasaman.ataei.200483@gmail.com>
2025-10-15 22:28:42 -0700

    Fixed the format in the logging section.

--- api.py
+++ api.py
@@ -9,7 +9,7 @@ import logging
 
 logging.basicConfig(
     level=logging.INFO,
-    format='%(asctime)s - %(levelname) - %(message)',
+    format='%(asctime)s - %(levelname)s - %(message)s',
     handlers=[logging.StreamHandler(sys.stdout)]
 )
 
